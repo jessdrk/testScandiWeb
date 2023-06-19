13a5a513bc12e04b888619d6334fa5b0ea0631e3
jessway <sofronova.1999@mail.ru>
2023-06-19 15:46:23 +0600

    rename folder

---
+++ frontend/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss">
+body {
+  background-image: url(./assets/bg.png);
+}
+
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@500&display=swap');
+</style>
---
+++ frontend/src/components/ActionCard.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="home__card">
+    <input 
+      class="home__card__input delete-checkbox" 
+      type="checkbox"
+      :checked="checked"
+      @change="$emit('checked', id, $event.target.checked)"
+    >
+    <div class="home__card__group">
+      <h1 class="home__card__titles">
+        {{ sku }}
+      </h1>
+      <h1 class="home__card__titles">
+        {{ name }}
+      </h1>
+      <h1 class="home__card__titles">
+        {{ price }} {{ priceIn }}
+      </h1>
+      <h1 v-if="type === 'DVD'" class="home__card__titles">
+        {{ titleSize }} {{ valueProduct }} {{ sizeIn }}
+      </h1>
+      <h1 v-if="type === 'Book'" class="home__card__titles">
+        {{ titleWeight }} {{ valueProduct }} {{ weightIn }}
+      </h1>
+      <h1 v-if="type === 'Furniture'" class="home__card__titles">
+        {{ titleDimension }} {{ valueProduct }}
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: String,
+      required: true
+    },
+    sku: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    price: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String,
+      required: true
+    },
+    valueProduct: {
+      type: String,
+      required: true
+    },
+    modelValue: {
+      type: Array,
+      default() {
+        return [];
+      }
+    },
+    checked: Boolean
+  },
+  emits: ['checked'],
+  data() {
+    return {
+      priceIn: '$',
+      titleSize: 'Size:',
+      titleWeight: 'Weight:',
+      titleDimension: 'Dimension:',
+      sizeIn: 'MB',
+      weightIn: 'KG'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.home__card {
+  width: 19vw;
+  height: 11vw;
+  border: 2px solid rgb(96, 138, 187);
+  margin-top: 2vw;
+  background-color: white;
+  border-radius: 5%;
+  &__input {
+    margin: 0.6vw 0 0 1.2vw;
+    width: 1.2vw;
+    height: 1.2vw;
+  }
+  &__group {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+  &__titles {
+    font-size: 20px;
+    margin: 0.1vw;
+    font-family: 'Open Sans', sans-serif;
+    font-weight: 400;
+  }
+  
+}
+</style>
